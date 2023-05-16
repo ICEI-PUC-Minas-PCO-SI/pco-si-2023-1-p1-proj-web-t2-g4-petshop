69c8ee74e80663170f93788c361d0f04ff4bf7f7
Carolina Baião <carolinaabpessoa@gmail.com>
2023-05-16 19:37:46 -0300

    Finalizando páginas de cadastro e login

--- src/tela2/cadastro_pessoas.js
+++ src/tela2/cadastro_pessoas.js
@@ -39,3 +39,46 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
   }
 });
+
+
+// manipulando os inputs radio 
+const pessoaFisicaRadio = document.getElementById("pessoa-fisica");
+const pessoaJuridicaRadio = document.getElementById("pessoa-juridica");
+
+const formularioPessoaFisica = document.getElementById("form-pessoa-fisica");
+const formularioPessoaJuridica = document.getElementById("form-pessoa-juridica");
+
+pessoaFisicaRadio.addEventListener("click", function() {
+  if (document.getElementById("pessoa-fisica").checked) {
+    document.getElementById("pessoa-juridica").checked = false;
+}
+  formularioPessoaFisica.style.display = "flex";
+  formularioPessoaJuridica.style.display = "none";
+});
+
+pessoaJuridicaRadio.addEventListener("click", function() {
+  if (document.getElementById("pessoa-juridica").checked) {
+    document.getElementById("pessoa-fisica").checked = false;
+    formularioPessoaFisica.style.display = "none";
+  formularioPessoaJuridica.style.display = "flex";
+}
+});
+
+// validando o botao isento de ie (!!!)
+const checkboxIsento = document.getElementById("isentoIE");
+const inscricaoEstadual = document.getElementById("inscricao-estadual");
+const estadoInscricao = document.getElementById("estado-inscricao");
+
+checkboxIsento.addEventListener("change", function() {
+  if(checkboxIsento.checked) {
+    inscricaoEstadual.setAttribute('disabled', 'disabled');
+    inscricaoEstadual.style.backgroundColor = "#A9A9A9";
+    estadoInscricao.setAttribute('disabled', 'disabled');
+    estadoInscricao.style.backgroundColor = "#A9A9A9";
+  } else {
+    inscricaoEstadual.removeAttribute('disabled');
+    inscricaoEstadual.style.backgroundColor = "initial";
+    estadoInscricao.removeAttribute('disabled');
+    estadoInscricao.style.backgroundColor = "initial";
+  }
+});
